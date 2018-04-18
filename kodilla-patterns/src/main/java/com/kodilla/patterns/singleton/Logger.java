3bedc035a9e519ae85aea89fed4be78e55f36b97
pskibinski <skibinskipawel@hotmail.com>
2018-04-18 08:50:27 +0200

    Zadanie 11.1 poprawka

--- kodilla-patterns/src/main/java/com/kodilla/patterns/singleton/Logger.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/singleton/Logger.java
@@ -6,6 +6,9 @@ public class Logger {
 
     private static Logger loggerInstance = null;
 
+    private Logger() {
+    }
+
     public static Logger getInstance() {
         if(loggerInstance == null) {
             synchronized (Logger.class) {
